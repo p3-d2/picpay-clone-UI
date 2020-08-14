9ff4ff8fc9fb711ea2f4a40d8efdce12338d7d66
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-14 14:24:51 -0300

    creating and styling the Header component Header the home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+
+import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles'
+
+import Suggestions from '../../components/Suggestions'
 
 export default function Home() {
   return (
-    <Text>Home</Text>
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name='qrcode-scan' size={30} color='#10C86E' />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name='gift' size={30} color='#10C86E' />
+        </Header>
+
+        <Suggestions />
+      </Container>
+    </Wrapper>
   )
 }
 
