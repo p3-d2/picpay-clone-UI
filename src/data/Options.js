eaf8ecd8f97430ae12af3df03d856a1c5742f0fb
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-15 18:36:24 -0300

    placing keys in the payment screen information and separating the payment screen information

---
+++ src/data/Options.js
@@ -0,0 +1,107 @@
+import img1 from '../../assets/01.png'
+import img5 from '../../assets/05.png'
+import img6 from '../../assets/06.png'
+import img7 from '../../assets/07.png'
+import img8 from '../../assets/08.png'
+
+export const OptionsServices = [
+  {
+    key: String(Math.random()),
+    img: img5,
+    title: 'Pagar Conta',
+    description: 'Paque em máquinas Cielo, Getnet e Rede escaneando o Qrcode'
+  },
+  {
+    key: String(Math.random()),
+    img: img6,
+    title: 'Pagar nas Maquininhas',
+    description: 'Paque sua conta de luz, água, boletos bancários, etc'
+  },
+  {
+    key: String(Math.random()),
+    img: img7,
+    title: 'Cobrar',
+    description: 'Cobre um amigo'
+  },
+  {
+    key: String(Math.random()),
+    img: img1,
+    title: 'Recarga de celular',
+    description: 'Vivo, Claro, Tim, Oi, Nextel, Algar, Sercomtel, Correios'
+  },
+]
+
+export const OptionsContatos = [
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa1',
+    description: 'nome de pessoa1'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa2',
+    description: 'nome de pessoa2'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa3',
+    description: 'nome de pessoa3'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa4',
+    description: 'nome de pessoa4'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa5',
+    description: 'nome de pessoa5'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa6',
+    description: 'nome de pessoa6'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa7',
+    description: 'nome de pessoa7'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa8',
+    description: 'nome de pessoa8'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa9',
+    description: 'nome de pessoa9'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa10',
+    description: 'nome de pessoa10'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa11',
+    description: 'nome de pessoa11'
+  },
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: '@pessoa12',
+    description: 'nome de pessoa12'
+  }
+]
