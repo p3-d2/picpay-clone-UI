a23bd21999ece95bde19305195f9f0a698af50ae
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-15 17:45:59 -0300

    separating data from the Home component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -9,104 +9,12 @@ import Tips from '../../components/Tips'
 import Banner from '../../components/Banner'
 
 import img8 from '../../../assets/08.png'
-import img9 from '../../../assets/09.png'
-import img10 from '../../../assets/10.png'
-import img11 from '../../../assets/11.png'
-import img12 from '../../../assets/12.png'
 
 import img13 from '../../../assets/13.png'
 
 import avatar from '../../../assets/avatar.png'
 
-const CarrouselOne = [
-  {
-    key: String(Math.random()),
-    img: img8,
-    title: 'Paque suas contas sem sair de casa',
-    bgColor: '#172C4A'
-  },
-  {
-    key: String(Math.random()),
-    img: img9,
-    title: 'Recarreque seu celular',
-    bgColor: '#6A0159'
-  },
-  {
-    key: String(Math.random()),
-    img: img10,
-    title: 'Paque um amigo à distância',
-    bgColor: '#4139C8'
-  },
-  {
-    key: String(Math.random()),
-    img: img11,
-    title: 'Saiba como pagar nas maquininhas',
-    bgColor: '#00Ab4B'
-  },
-  {
-    key: String(Math.random()),
-    img: img12,
-    title: 'Saiba onde pagar lojas online com PicPay',
-    bgColor: '#BA2f76'
-  }
-]
-
-const CarrouselTwo = [
-  {
-    key: String(Math.random()),
-    img: img12,
-    title: 'Saiba onde pagar com o PicPay',
-    bgColor: '#BA2f76'
-  },
-  {
-    key: String(Math.random()),
-    img: img11,
-    title: 'Paque suas contas sem sair de casa',
-    bgColor: '#00Ab4B'
-  },
-  {
-    key: String(Math.random()),
-    img: img10,
-    title: 'Paque parcelado no cartão de crédito',
-    bgColor: '#4139C8'
-  },
-  {
-    key: String(Math.random()),
-    img: img9,
-    title: 'Saiba como pagar nas maquininhas',
-    bgColor: '#6A0159'
-  },
-  {
-    key: String(Math.random()),
-    img: img10,
-    title: 'Saiba onde pagar lojas online com PicPay',
-    bgColor: '#172C4A'
-  },
-  {
-    key: String(Math.random()),
-    img: img11,
-    title: 'Saiba como pagar um boleto',
-    bgColor: '#6A0159'
-  },
-  {
-    key: String(Math.random()),
-    img: img12,
-    title: 'Adicione dinheiro no PicPay',
-    bgColor: '#4139C8'
-  },
-  {
-    key: String(Math.random()),
-    img: img11,
-    title: 'Recarreque seu cartão de transporte em casa',
-    bgColor: '#00Ab4B'
-  },
-  {
-    key: String(Math.random()),
-    img: img10,
-    title: 'Compre o que quiser sem sair de casa',
-    bgColor: '#BA2f76'
-  },
-]
+import { CarrouselOne, CarrouselTwo } from '../../data/Carrousel'
 
 export default function Home() {
   return (
