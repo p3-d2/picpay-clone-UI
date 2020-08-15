2160c084df897fb0b686ed625679621661a3387b
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-15 17:55:06 -0300

    separating data from the Suggestions component

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -2,56 +2,12 @@ import React from 'react'
 
 import { Container, Option, Img, Label } from './styles.js'
 
-import img1 from '../../../assets/01.png'
-import img2 from '../../../assets/02.png'
-import img3 from '../../../assets/03.png'
-import img4 from '../../../assets/04.png'
-import img5 from '../../../assets/05.png'
-import img6 from '../../../assets/06.png'
-import img7 from '../../../assets/07.png'
-
-const items = [
-  {
-    key: String(Math.random()),
-    img: img1,
-    label: 'Recarga'
-  },
-  {
-    key: String(Math.random()),
-    img: img2,
-    label: 'Uber'
-  },
-  {
-    key: String(Math.random()),
-    img: img3,
-    label: 'Ônibus'
-  },
-  {
-    key: String(Math.random()),
-    img: img4,
-    label: 'Tv'
-  },
-  {
-    key: String(Math.random()),
-    img: img5,
-    label: 'Doações'
-  },
-  {
-    key: String(Math.random()),
-    img: img6,
-    label: 'Barras'
-  },
-  {
-    key: String(Math.random()),
-    img: img7,
-    label: 'Faq'
-  }
-]
+import { SuggestionsItems } from '../../data/Suggestions'
 
 export default function Suggestions() {
   return (
     <Container>
-      {items.map(item => (
+      {SuggestionsItems.map(item => (
         <Option key={item.key}>
           <Img source={item.img} />
           <Label>{item.label}</Label>
